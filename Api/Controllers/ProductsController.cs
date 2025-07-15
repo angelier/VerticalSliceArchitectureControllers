2ee363e926357fe9b57cd64815ae05e3c4a20615
aramirez <aramirez@midasconsultores.com.ar>
2025-07-15 11:13:52 -0300

    prueba de commit 5

--- Api/Controllers/ProductsController.cs
+++ Api/Controllers/ProductsController.cs
@@ -61,7 +61,10 @@ namespace Api.Controllers
              case 3:
                              Console.WriteLine("Case 3 executed");
                     break;
-  
+                case 4:
+                                    Console.WriteLine("Case 4 executed");
+                    Console.WriteLine("Case 4 executed again");
+                                 break;
                 default:
                     break;
             }
